38f49366fbcf1bbb81f589da0338426c25cb9784
@OutKast317 <phonewinttun173@gmail.com>
2025-08-23 12:03:07 +0630

    database connection

--- PWTDotNetTrainingInPerson.ConsoleApp/Program.cs
+++ PWTDotNetTrainingInPerson.ConsoleApp/Program.cs
@@ -1,4 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
+
+// 'Ctrl + .' on SqlConnectionStringBuilder
+
+// comment - Ctrl + K + C
+// uncomment - Ctrl + K + U
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = ".";
+sqlConnectionStringBuilder.InitialCatalog = "PWTDotNetTrainingInPerson";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sasa@123";
+sqlConnectionStringBuilder.TrustServerCertificate = true;
+
+Console.WriteLine(sqlConnectionStringBuilder.ConnectionString);
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+
+Console.WriteLine("Opening connection...");
+connection.Open();
+Console.WriteLine("Connection opened.");
+
+Console.WriteLine("Closing connection...");
+connection.Close();
+Console.WriteLine("Connection closed.");
+
 Console.ReadKey();
-Console.ReadLine();
