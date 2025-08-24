f17df6adbe0ea374c489b7362d16a7f655f95f6c
@OutKast317 <phonewinttun173@gmail.com>
2025-08-24 12:35:45 +0630

    CRUD

---
+++ PWTDotNetTrainingInPerson.ConsoleApp/AdoDotNetService.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWTDotNetTrainingInPerson.ConsoleApp
+{
+    internal class AdoDotNetService
+    {
+        SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "PWTDotNetTrainingInPerson",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true,
+        };
+
+        public void Read()
+        {
+
+            SqlConnection connection = new SqlConnection(sb.ConnectionString);
+            connection.Open();
+            //connection အဖွင့်အပိတ်ကြားမှာ query တွေအလုပ်လုပ်မယ်
+
+            string query = @"SELECT [StudentID]
+      ,[StudentName]
+      ,[FatherName]
+      ,[DateOfBirth]
+      ,[Address]
+      ,[MobileNumber]
+      ,[DeleteFlag]
+  FROM [dbo].[tbl_students]";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+        }
+
+        public void Create() {
+            SqlConnection connection = new SqlConnection(sb.ConnectionString);
+            connection.Open();
+            //connection အဖွင့်အပိတ်ကြားမှာ query တွေအလုပ်လုပ်မယ်
+
+            string query = @"INSERT INTO [dbo].[tbl_students]
+           ([StudentName]
+           ,[FatherName]
+           ,[DateOfBirth]
+           ,[Address]
+           ,[MobileNumber]
+           ,[DeleteFlag])
+     VALUES
+           (<StudentName, nvarchar(100),>
+           ,<FatherName, nvarchar(100),>
+           ,<DateOfBirth, datetime,>
+           ,<Address, nvarchar(255),>
+           ,<MobileNumber, nvarchar(20),>
+           ,<DeleteFlag, bit,>)";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            string message = result > 0 ? "Insert successful" : "Insert failed";
+            Console.WriteLine(message);
+        }
+
+        public void Update() {
+            SqlConnection connection = new SqlConnection(sb.ConnectionString);
+            connection.Open();
+            //connection အဖွင့်အပိတ်ကြားမှာ query တွေအလုပ်လုပ်မယ်
+
+            string query = @"UPDATE [dbo].[tbl_students]
+   SET [StudentName] = <StudentName, nvarchar(100),>
+      ,[FatherName] = <FatherName, nvarchar(100),>
+      ,[DateOfBirth] = <DateOfBirth, datetime,>
+      ,[Address] = <Address, nvarchar(255),>
+      ,[MobileNumber] = <MobileNumber, nvarchar(20),>
+      ,[DeleteFlag] = <DeleteFlag, bit,>
+ WHERE <Search Conditions,,>";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            string message = result > 0 ? "Update successful" : "Update failed";
+            Console.WriteLine(message);
+        }
+ 
+        public void Delete() {
+            SqlConnection connection = new SqlConnection(sb.ConnectionString);
+            connection.Open();
+            //connection အဖွင့်အပိတ်ကြားမှာ query တွေအလုပ်လုပ်မယ်
+
+            string query = @"UPDATE FROM [dbo].[tbl_students]
+SET [DeleteFlag] = 1
+      WHERE <Search Conditions,,>";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            int result = cmd.ExecuteNonQuery();
+
+            connection.Close();
+            string message = result > 0 ? "Delete successful" : "Delete failed";
+            Console.WriteLine(message);
+        }
+    }
+}
--- PWTDotNetTrainingInPerson.ConsoleApp/Program.cs
+++ PWTDotNetTrainingInPerson.ConsoleApp/Program.cs
@@ -1,30 +1,74 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Data.SqlClient;
-
-Console.WriteLine("Hello, World!");
+using System.Data;
+using System.Net.Http.Headers;
 
 // 'Ctrl + .' on SqlConnectionStringBuilder
 
 // comment - Ctrl + K + C
 // uncomment - Ctrl + K + U
 
-SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
-sqlConnectionStringBuilder.DataSource = ".";
-sqlConnectionStringBuilder.InitialCatalog = "PWTDotNetTrainingInPerson";
-sqlConnectionStringBuilder.UserID = "sa";
-sqlConnectionStringBuilder.Password = "sasa@123";
-sqlConnectionStringBuilder.TrustServerCertificate = true;
+//SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+//sqlConnectionStringBuilder.DataSource = ".";
+//sqlConnectionStringBuilder.InitialCatalog = "PWTDotNetTrainingInPerson";
+//sqlConnectionStringBuilder.UserID = "sa";
+//sqlConnectionStringBuilder.Password = "sasa@123";
+//sqlConnectionStringBuilder.TrustServerCertificate = true;
 
-Console.WriteLine(sqlConnectionStringBuilder.ConnectionString);
+//Console.WriteLine(sqlConnectionStringBuilder.ConnectionString);
 
-SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+//SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
 
-Console.WriteLine("Opening connection...");
-connection.Open();
-Console.WriteLine("Connection opened.");
+//Console.WriteLine("Opening connection...");
+//connection.Open();
+//Console.WriteLine("Connection opened.");
 
-Console.WriteLine("Closing connection...");
-connection.Close();
-Console.WriteLine("Connection closed.");
+//string query = @"SELECT [StudentID]
+//      ,[StudentName]
+//      ,[FatherName]
+//      ,[DateOfBirth]
+//      ,[Address]
+//      ,[MobileNumber]
+//      ,[DeleteFlag]
+//  FROM [dbo].[tbl_students]
+//";
 
-Console.ReadKey();
+//SqlCommand cmd = new SqlCommand(query, connection); // to connect command with connection
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);// to fetch data from database
+//DataTable dt = new DataTable(); // in-memory representation of data
+
+//Console.WriteLine("Closing connection...");
+//connection.Close();
+//Console.WriteLine("Connection closed.");
+
+//Console.ReadKey();
+
+//SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder() {
+//    DataSource = ".",
+//    InitialCatalog = "PWTDotNetTrainingInPerson",
+//    UserID = "sa",
+//    Password = "sasa@123",
+//    TrustServerCertificate = true,
+//};
+
+//SqlConnection connection = new SqlConnection(sb.ConnectionString);
+//connection.Open();
+
+////connection အဖွင့်အပိတ်ကြားမှာ query တွေအလုပ်လုပ်မယ်
+
+//string query = @"SELECT [StudentID]
+//      ,[StudentName]
+//      ,[FatherName]
+//      ,[DateOfBirth]
+//      ,[Address]
+//      ,[MobileNumber]
+//      ,[DeleteFlag]
+//  FROM [dbo].[tbl_students]";
+
+//SqlCommand cmd = new SqlCommand(query, connection);
+//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+//DataTable dt = new DataTable();
+//adapter.Fill(dt);
+
+
+//connection.Close();
